291c278ec58327bd7570e2bc23373c4b202aee28
sparksdev <dev@sparks.dev>
2023-01-18 21:31:36 -0500

    remove placeholders

--- stories/design-system/Overview.stories.js
+++ stories/design-system/Overview.stories.js
@@ -4,15 +4,12 @@ export default {
   title: 'Design System/Overview',
 };
 
-const Template = () => {
+export const FigmaBoard = () => {
   return `
     <div style="display: flex; justify-content: stretch; align-items: stretch; position: absolute; width: 100%; height: 100%; left: 0; top: 0;">
-      <iframe style="border: 0px; flex-grow: 1;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FbKI0SvJirPHvOLEnuEcSmt%2FSPARKS-Design-System%3Fnode-id%3D0%253A1%26t%3DbxuIM2NlzD4rT5Yl-1" allowfullscreen></iframe>
+      <iframe style="border: 0px; flex-grow: 1;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FbKI0SvJirPHvOLEnuEcSmt%2FDesign-System%3Fnode-id%3D0%253A1%26t%3Dv94Z4A49dkfTqB72-1" allowfullscreen></iframe>
     </div>
   `;
-};
+}
+FigmaBoard.parameters = {	options: { showPanel: false } }
 
-export const Primary = Template.bind({});
-Primary.parameters = {
-	options: { showPanel: false },
-};
