89432ef5b1c0e83b769887ac10784244f22a4c22
7i7o <mparij@gmail.com>
2023-01-21 03:42:04 -0300

    build menu / fix link footer / add avatar asset

---
+++ stories/components/Menu.stories.js
@@ -0,0 +1,73 @@
+import '../../css/index.js';
+import avatar from '../../assets/utils/avatar.png';
+
+export default {
+  title: 'Components/Menu',
+  state: {
+    control: { type: 'select' },
+    options: ['closed', 'open'],
+    description: 'State of the Menu'
+  },
+  content: {
+    control: { type: 'select' },
+    options: ['empty', 'selected'],
+    description: 'State of the Selection'
+  },
+};
+
+const Template = ({ state, content }) => {
+  if (state === 'closed' && content === 'empty') return `
+    <div class='menu'>
+      <div class='menu--header'>
+        <div class='sandwich'><input type="checkbox" /><span></span><span></span><span></span></div>
+      </div>
+    </div>
+  `
+  if (state === 'closed' && content === 'selected') return `
+    <div class='menu'>
+      <div class='menu--header'>
+        <div class='avatar'><img src=${avatar} /></div>
+        <div class='menu--header-title'>Notifications</div>
+        <div class='sandwich'><input type="checkbox" /><span></span><span></span><span></span></div>
+      </div>
+    </div>
+  `
+  if (state === 'open' && content === 'empty') return `
+    <div class='menu'>
+      <div class='menu--header'>
+        <div class='sandwich'><input type="checkbox" /><span></span><span></span><span></span></div>
+      </div>
+      <ul class='menu--content'>
+        <a href='#'><li>Dashboard</li></a>
+        <a href='#'><li>Account</li></a>
+        <a href='#'><li>Marketplace</li></a>
+      </ul>
+    </div>
+  `
+  if (state === 'open' && content === 'selected') return `
+    <div class='menu'>
+      <div class='menu--header'>
+        <div class='avatar'><img src=${avatar} /></div>
+        <div class='menu--header-title'>Notifications</div>
+        <div class='sandwich'><input type="checkbox" /><span></span><span></span><span></span></div>
+      </div>
+      <ul class='menu--content'>
+        <a href='#'><li>Dashboard</li></a>
+        <a href='#'><li>Account</li></a>
+        <a href='#'><li>Marketplace</li></a>
+      </ul>
+    </div>
+  `
+};
+
+export const Default = Template.bind({});
+Default.args = { state: 'closed', content: 'empty' };
+
+export const Closed = Template.bind({});
+Closed.args = { state: 'closed', content: 'selected' };
+
+export const Open = Template.bind({});
+Open.args = { state: 'open', content: 'empty' };
+
+export const OpenSelected = Template.bind({});
+OpenSelected.args = { state: 'open', content: 'selected' };
